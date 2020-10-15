fc9460b7289df3b8209873c950a56be22f1bc25a
3bdo3mer <55184522+3bdo3mer@users.noreply.github.com>
2020-10-15 05:40:09 +0200

    finshed recycler view

---
+++ app/src/main/java/com/example/easytripplanner/Trip.java
@@ -0,0 +1,120 @@
+package com.example.easytripplanner;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Trip {
+
+    //primary keys (userID, id).
+    private int userID;
+    private int tripID;
+    private String tripName;
+    private String StartPoint;
+    private String endPoint;
+    private Date startDate;
+    private Time startTime;
+    private ArrayList<String> notes;
+    private String repeater;
+    private String tripType;
+    private String status;
+
+    public Trip(String tripName, String startPoint, String endPoint, Date startDate, Time startTime, String status) {
+        this.tripName = tripName;
+        StartPoint = startPoint;
+        this.endPoint = endPoint;
+        this.startDate = startDate;
+        this.startTime = startTime;
+        this.status = status;
+    }
+
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public int getTripID() {
+        return tripID;
+    }
+
+    public void setTripID(int tripID) {
+        this.tripID = tripID;
+    }
+
+    public String getTripName() {
+        return tripName;
+    }
+
+    public void setTripName(String tripName) {
+        this.tripName = tripName;
+    }
+
+    public String getStartPoint() {
+        return StartPoint;
+    }
+
+    public void setStartPoint(String startPoint) {
+        StartPoint = startPoint;
+    }
+
+    public String getEndPoint() {
+        return endPoint;
+    }
+
+    public void setEndPoint(String endPoint) {
+        this.endPoint = endPoint;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Time getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Time startTime) {
+        this.startTime = startTime;
+    }
+
+    public ArrayList<String> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(ArrayList<String> notes) {
+        this.notes = notes;
+    }
+
+    public String getRepeater() {
+        return repeater;
+    }
+
+    public void setRepeater(String repeater) {
+        this.repeater = repeater;
+    }
+
+    public String getTripType() {
+        return tripType;
+    }
+
+    public void setTripType(String tripType) {
+        this.tripType = tripType;
+    }
+}
+
---
+++ app/src/main/java/com/example/easytripplanner/TripAdapter.java
@@ -0,0 +1,129 @@
+package com.example.easytripplanner;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class TripAdapter extends RecyclerView.Adapter<TripAdapter.MyViewHolder>{
+
+    Context context;
+    ArrayList<Trip> trips;
+
+
+    public TripAdapter(UpComingFragment context, ArrayList<Trip> trips) {
+        this.context = context;
+        this.trips = trips;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.custom_row,parent,false);
+        MyViewHolder myViewHolder = new MyViewHolder(view);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        final Trip trip = trips.get(position);
+        holder.date.setText((CharSequence) trip.getStartDate());
+        holder.time.setText((CharSequence) trip.getStartTime());
+        holder.name.setText(trip.getTripName());
+        holder.status.setText(trip.getStatus());
+        holder.startpoint.setText(trip.getStartPoint());
+        holder.endpoint.setText(trip.getEndPoint());
+        holder.start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //code to make alert dialog and notification
+            }
+        });
+        holder.notes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //code to show all notes the user enter it
+            }
+        });
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String name = trip.getTripName();
+                String status = trip.getStatus();
+                Date date = trip.getStartDate();
+                Time time = trip.getStartTime();
+                String startPoint = trip.getStartPoint();
+                String endPoint = trip.getEndPoint();
+                ArrayList<String> note = trip.getNotes();
+
+                //delete MainActivity2 and put instead of it the name of the edit activity
+
+          /*    Intent intent =new Intent(context,MainActivity2.class);
+                intent.putExtra("Name", name);
+                intent.putExtra("Status", status);
+                intent.putExtra("Date", date);
+                intent.putExtra("Time", time);
+                intent.putExtra("StartPoint", startPoint);
+                intent.putExtra("EndPoint", endPoint);
+                intent.putExtra("Notes", note);
+                context.startActivity(intent);
+          */
+
+               // in the edit activity you will write this code
+          /*
+                String name = getIntent().getStringExtra("Name");
+                String status = getIntent().getStringExtra("Status");
+                String date = getIntent().getStringExtra("Date");
+                String time = getIntent().getStringExtra("Time");
+                String startPoint = getIntent().getStringExtra("StartPoint");
+                String endPoint = getIntent().getStringExtra("EndPoint");
+                 ArrayList<String> note = getIntent().getStringArrayListExtra("Notes");
+
+                // and in every edit text will write
+                //edittext.setText("name one of the above variable");
+         */
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return trips.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView date , time , name, status, startpoint, endpoint;
+        ImageView notes;
+        Button start;
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+                date = itemView.findViewById(R.id.text1date);
+                time = itemView.findViewById(R.id.text2time);
+                name = itemView.findViewById(R.id.text3name);
+                status = itemView.findViewById(R.id.text4status);
+                startpoint = itemView.findViewById(R.id.text5startpoint);
+                endpoint = itemView.findViewById(R.id.text6endpoint);
+                notes = itemView.findViewById(R.id.show_notes);
+                start = itemView.findViewById(R.id.start_trip_customrow);
+            }
+        }
+    }
+
